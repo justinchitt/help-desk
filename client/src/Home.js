d4c81a6d0cb63b78bcbab9331c930d104eb564ae
Justin Chittarath <justinchitt@gmail.com>
2022-01-05 21:02:14 -0700

    minor change to company

--- client/src/Home.js
+++ client/src/Home.js
@@ -19,7 +19,7 @@ function Home({user}) {
         <div>
             <Card className="alltickets" style={{width: "70rem"}}>
                 <Card.Header as="h3">My home</Card.Header>
-                <Card.Text id="bold" className="center">Welcome {`${user.first_name} ${user.last_name} from ${company.name},`}</Card.Text>
+                <Card.Text id="bold" className="center">Welcome {`${user.first_name} ${user.last_name}`}{company?` from ${company.name}`:null},</Card.Text>
                 <Card.Text className="center">What would you like to do today?</Card.Text>
                 {user.admin === true?<Link to="/tickets" className="center">View Tickets</Link>:<Link className="center" to="/ticketform">Submit a ticket</Link>}
                 {user.admin === true?<Link to="/claimed" className="center">View your claimed tickets</Link>:<Link to="/viewall" className="center">View your tickets</Link>}
